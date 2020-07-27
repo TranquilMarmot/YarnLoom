ec0c7861138241a1fd4b6a2fe36b55ac4634abc7
TranquilMarmot <nate272@gmail.com>
2020-07-26 19:27:34 -0700

    Handle new search options in node

--- loom-editor/src/components/NodeGraphView/index.tsx
+++ loom-editor/src/components/NodeGraphView/index.tsx
@@ -8,6 +8,8 @@ import {
   getSearchingBody,
   getSearchingTags,
   getSearchString,
+  getCaseSensitivityEnabled,
+  getRegexEnabled,
 } from "../../state/Selectors";
 
 import { YarnGraphNode } from "../NodeGraph";
@@ -92,6 +94,85 @@ const renderBody = (
   return <NodeBody body={body} />;
 };
 
+/**
+ * Returns true if the given string matches the given search string.
+ *
+ * @param test String to test
+ * @param searchString Current search string
+ * @param caseSensitive Whether or not the search should be case sensitive
+ * @param regexEnabled Whether or not regex is enabled
+ */
+const stringMatchesSearch = (
+  test: string,
+  searchString: string,
+  caseSensitive: boolean,
+  regexEnabled: boolean
+): boolean => {
+  if (regexEnabled) {
+    // "i" flag indicates a case insensitive search, so we only include it if we're NOT being case sensitive
+    const regexFlags = `g${caseSensitive ? "" : "i"}`;
+    const regex = new RegExp(searchString, regexFlags);
+
+    return regex.test(test);
+  }
+
+  // includes is case sensitive by default
+  if (caseSensitive) {
+    return test.includes(searchString);
+  }
+
+  return test.toLowerCase().includes(searchString.toLowerCase());
+};
+
+/**
+ * Returns true if the current node should be considered "searched".
+ * A "searched" node is rendered with full opacity, otherwise it is rendered slightly transparent.
+ *
+ * @param node The current node
+ * @param searchString String being searched for
+ * @param searchingTitle Whether or not to include the title of the node
+ * @param searchingBody Whether or not to include the body of the node
+ * @param searchingTags Whether or not to include the node's tags
+ * @param caseSensitive Whether or not the search should be case sensitive
+ * @param regexEnabled Whether or not the search should use regex
+ */
+const isSearched = (
+  { title, body, tags }: YarnNode,
+  searchString: string,
+  searchingTitle: boolean,
+  searchingBody: boolean,
+  searchingTags: boolean,
+  caseSensitive: boolean,
+  regexEnabled: boolean
+): boolean => {
+  // no search active
+  if (!searchingTitle && !searchingBody && !searchingTags) {
+    return true;
+  }
+
+  let searched = false;
+
+  if (searchingTitle) {
+    searched =
+      searched ||
+      stringMatchesSearch(title, searchString, caseSensitive, regexEnabled);
+  }
+
+  if (searchingBody) {
+    searched =
+      searched ||
+      stringMatchesSearch(body, searchString, caseSensitive, regexEnabled);
+  }
+
+  if (searchingTags) {
+    searched =
+      searched ||
+      stringMatchesSearch(tags, searchString, caseSensitive, regexEnabled);
+  }
+
+  return searched;
+};
+
 const NodeGraphView: FunctionComponent<NodeGraphViewProps> = ({
   node: { yarnNode },
 }) => {
@@ -99,7 +180,7 @@ const NodeGraphView: FunctionComponent<NodeGraphViewProps> = ({
   const [colorChooserOpen, setColorChooserOpen] = useState(false);
   const [tagChooserOpen, setTagChooserOpen] = useState(false);
 
-  const { colorID, title, body, tags } = yarnNode;
+  const { colorID, title } = yarnNode;
 
   if (!state) {
     return null;
@@ -108,15 +189,21 @@ const NodeGraphView: FunctionComponent<NodeGraphViewProps> = ({
   const searchingTitle = getSearchingTitle(state);
   const searchingBody = getSearchingBody(state);
   const searchingTags = getSearchingTags(state);
+  const caseSensitivityEnabled = getCaseSensitivityEnabled(state);
+  const regexEnabled = getRegexEnabled(state);
   const searchString = getSearchString(state);
 
   // if we're searching for something, and this node matches that something,
   // then this will be true... if this is false, the node is rendered as "dimmed"
-  const searched =
-    (!searchingTitle && !searchingBody && !searchingTags) || // no search active
-    (searchingTitle && title.includes(searchString)) ||
-    (searchingBody && body.includes(searchString)) ||
-    (searchingTags && tags.includes(searchString));
+  const searched = isSearched(
+    yarnNode,
+    searchString,
+    searchingTitle,
+    searchingBody,
+    searchingTags,
+    caseSensitivityEnabled,
+    regexEnabled
+  );
 
   return (
     <div
